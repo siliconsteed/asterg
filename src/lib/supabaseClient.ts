4fc8aedf3ad531b758441c99ec68abab4cde3179
vsriranga <pc@vsrirangas-MacBook-Air.local>
2026-01-06 00:29:16 +0530

    Supabase comments released.

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -5,11 +5,11 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://rrmmhfuyoje
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseAnonKey) {
- // throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
+    throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
 }
 
 if (!supabaseUrl) {
- // throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+    throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
 }
 
-//export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
